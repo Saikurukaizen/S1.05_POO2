cd90d8315b7919360af854d9b72a989ba6249927
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 19:43:28 +0200

    Delete Lv.1/S1.05_lv.1_gat.php

--- Lv.1/S1.05_lv.1_gat.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-class Gat extends Animal{
-    
-    public function makeSound(): void{
-        echo $this->tipoAnimal. " ha maullado: Miau!" . PHP_EOL;
-    }
-}
