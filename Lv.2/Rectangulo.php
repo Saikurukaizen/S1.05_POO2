5d53f7a97130d6046618923a85d3977249d3868e
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 20:11:43 +0200

    Update Rectangulo.php

--- Lv.2/Rectangulo.php
+++ Lv.2/Rectangulo.php
@@ -3,17 +3,10 @@ declare(strict_types=1);
 
 require_once './Shape.php';
 
-class Rectangulo implements Shape{
-    private float $baseRec;
-    private float $alturaRec;
-
-    public function __construct(float $baseRec, float $alturaRec){
-        $this->baseRec = $baseRec;
-        $this->alturaRec = $alturaRec;
-    }
+class Rectangulo extends Shape{
 
     public function calcularArea(): float{
-        return $this->baseRec * $this->alturaRec;
+        return $this->base * $this->altura;
     }
 }
 ?>
