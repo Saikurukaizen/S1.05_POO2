a59fbe8ab915a222c1eedf00a9526d04f58508ed
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 20:12:10 +0200

    Update Triangulo.php

--- Lv.2/Triangulo.php
+++ Lv.2/Triangulo.php
@@ -3,17 +3,10 @@ declare(strict_types=1);
 
 require_once './Shape.php';
 
-class Triangulo implements Shape{
-    private float $baseTri;
-    private float $alturaTri;
-
-    public function __construct(float $baseTri, float $alturaTri){
-        $this->baseTri = $baseTri;
-        $this->alturaTri = $alturaTri;
-    }
+class Triangulo extends Shape{
 
     public function calcularArea(): float{
-        return ($this->baseTri * $this->alturaTri) / 2;
+        return ($this->base * $this->altura) / 2;
     }
 }
 ?>
