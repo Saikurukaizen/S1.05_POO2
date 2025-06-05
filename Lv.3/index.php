2523c5a584e55f2f396a247afb1df31ff9dde82f
Marc Sanchez <163992267+Saikurukaizen@users.noreply.github.com>
2025-06-05 20:12:37 +0200

    Update index.php

--- Lv.3/index.php
+++ Lv.3/index.php
@@ -10,6 +10,6 @@ $rec1 = new Rectangulo(18, 12);
 $tri1 = new Triangulo(5, 12);
 $cir1 = new Circulo(5);
 
-echo "Área del rectángulo: " . $rec1->calcularArea() . "\n" . PHP_EOL;
-echo "Área del triángulo: " . $tri1->calcularArea() . "\n" . PHP_EOL;
-echo "Área del círculo: " . $cir1->calcularArea() . "\n" . PHP_EOL;
+foreach ($figuras as $figura) {
+    echo "Área: " . $figura->calcularArea() . PHP_EOL;
+}
